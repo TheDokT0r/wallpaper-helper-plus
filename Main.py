b32a3b38ee652cf1c17317bc88a588004d7f0e76
Orche <orkv1234@gmail.com>
2023-06-28 22:39:00 +0300

    init ui

--- Main.py
+++ Main.py
@@ -1,1 +1,19 @@
-print('Hello, World!')
+import tkinter as tk
+
+def main():
+    root = tk.Tk()
+    root.title("Hello World")
+    root.geometry("1280x720")
+
+    title = tk.Label(root, text="Wallpaper Helper Plus v0.1",
+                    font=("Helvetica", 16))
+    
+    
+
+
+    title.pack()
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
